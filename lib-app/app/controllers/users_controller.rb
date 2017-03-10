f5eb54c7125bfe176f49d128316586236149ef67
kkuo57 <kkuo507@gmail.com>
2017-03-10 15:20:58 -0800

    adds new form, adds bcrypt and adds part of show one user

--- lib-app/app/controllers/users_controller.rb
+++ lib-app/app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    new_user = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(new_user)
+    redirect_to root_path
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
 end
